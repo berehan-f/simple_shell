818a8609878c0fc8b94179d619eecf344c21c32f
Berehan <berehanf.hewot@gmail.com>
2024-02-29 12:58:17 +0300

    Update launch.c

--- launch.c
+++ launch.c
@@ -13,25 +13,23 @@ int launch(char **args, char **av)
 	pid_t pid;
 	int pid_status;
 
+	if (stat(args[0], &stat_buf) != 0)
+	{
+		perror(av[0]);
+		return (1);
+	}
 	pid = fork();
 	if (pid == -1)
 		error("Can't fork process:");
 	if (pid == 0)
 	{
-		if (stat(args[0], &stat_buf) == 0)
-		{
-			if (execve(args[0], args, environ) == -1)
-				error(av[0]);
-		}
-		else
-		{
+		if (execve(args[0], args, environ) == -1)
 			error(av[0]);
-		}
 	}
 	else
 	{
 		do {
-			pid_status = waitpid(pid, &pid_status, 0);
+			pid_status = waitpid(pid, &pid_status, WUNTRACED);
 		} while (!WIFEXITED(pid_status) && !WIFSIGNALED(pid_status));
 	}
 	return (1);
